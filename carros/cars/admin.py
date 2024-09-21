686ed1413b74e9846d7970baa598cd792a9c8e4b
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-09-21 17:44:44 -0300

    adicionando foreign key e nova migration

--- carros/cars/admin.py
+++ carros/cars/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car,Brand
 
+class BrandAdmin(admin.ModelAdmin):
+    list_display=('name',)
+    search_fields=('name',)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ('id','model','brand','factory_year','model_year','value') #caption
+    list_display = ('model','brand','factory_year','model_year','value') #caption
     search_fields=('model',)
-    
+        
 admin.site.register(Car, CarAdmin)
+admin.site.register(Brand,BrandAdmin)
--- carros/cars/models.py
+++ carros/cars/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
+class Brand(models.Model):
+    id= models.AutoField(primary_key=True)
+    name= models.CharField(max_length=200)
+    
+    def __str__(self) -> str:
+        return self.name
+
 class Car(models.Model):
     id = models.AutoField(primary_key=True) #auto increment
     model=models.CharField(max_length=200) #string
-    brand= models.CharField(max_length=200) #marca
+    brand= models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand') #marca
     factory_year=models.IntegerField(blank=True,null=True)
     model_year=models.IntegerField(blank=True,null=True)
     value=models.FloatField(blank=True,null=True) # float
     
+    def __str__(self):
+        return self.model
     
+
