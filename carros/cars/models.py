2c45bf955d3a7452d00c9c796154ff0f5874ef7c
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-09-21 12:32:35 -0300

    criando modelo cars e migração

--- carros/cars/models.py
+++ carros/cars/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    id = models.AutoField(primary_key=True) #auto increment
+    model=models.CharField(max_length=200) #string
+    brand= models.CharField(max_length=200) #marca
+    factory_year=models.IntegerField(blank=True,null=True)
+    model_year=models.IntegerField(blank=True,null=True)
+    value=models.FloatField(blank=True,null=True) # float
+    
+    
