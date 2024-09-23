557e81c8c9d3b9d3a4ba12fe151a84dd96888484
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-09-23 11:55:32 -0300

    requisição url

--- carros/cars/views.py
+++ carros/cars/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
-    cars=Car.objects.filter(factory_year=1)
+    # cars=Car.objects.filter(factory_year=1)
     # cars=Car.objects.filter(brand__name='bmw')
     # cars=Car.objects.filter(brand__contains='bm')
-    # cars=Car.objects.all()
+    
+    # print(request)
+    # print(request.GET.get('search'))
+    search=request.GET.get('search')
+    # print(search)
+    
+    if search:
+        cars=Car.objects.filter(model=search)
+    else:
+        cars=Car.objects.all()
     
     return render(
         request,
