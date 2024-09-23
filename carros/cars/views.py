61af03d610ea44396a9027bf554d26295df1b843
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-09-23 11:30:45 -0300

    utilizando ORM

--- carros/cars/views.py
+++ carros/cars/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
+from cars.models import Car
 
 def cars_view(request):
+    cars=Car.objects.filter(factory_year=1)
+    # cars=Car.objects.filter(brand__name='bmw')
+    # cars=Car.objects.filter(brand__contains='bm')
+    # cars=Car.objects.all()
+    
     return render(
         request,
         'cars.html', 
-        {'cars': {'model': 'Astra 2.0'} }
+        {'cars': cars }
     )
