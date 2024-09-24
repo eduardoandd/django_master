0b17c96c39607ff73c4420205117cc299ca532f1
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-09-24 09:36:10 -0300

    template dinamico e validações

--- carros/cars/forms.py
+++ carros/cars/forms.py
@@ -6,5 +6,17 @@ class CarModelForm(forms.ModelForm):
         model= Car
         fields= '__all__'
     
+    def clean_value(self):
+        value= self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('value', 'Valor mínimo de carro deve ser de R$20.0000')
+        
+        return value
     
+    def clean_factory_year(self):
+        factory_year=self.cleaned_data.get('factory_year')
+        if factory_year <= 1975:
+            self.add_error('factory_year', 'Data minimica excedida')
+        
+        return factory_year
     
