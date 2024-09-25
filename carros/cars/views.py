e26a71fc440692900492aa5cd12a57e0a148e4c1
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-09-25 17:05:03 -0300

    views genericas

--- carros/cars/views.py
+++ carros/cars/views.py
@@ -1,37 +1,37 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render,redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
+from django.views.generic import ListView,CreateView,DetailView
 
-def cars_view(request):
-    # cars=Car.objects.filter(factory_year=1)
-    # cars=Car.objects.filter(brand__name='bmw')
-    # cars=Car.objects.filter(brand__contains='bm')
-    
-    # print(request)
-    # print(request.GET.get('search'))
-    search=request.GET.get('search')
-    # print(search)
-    
-    if search:
-        cars=Car.objects.filter(model__icontains=search)
-    else:
-        cars=Car.objects.all()
-    
-    return render(
-        request,
-        'cars.html', 
-        {'cars': cars }
-    )
 
-def new_car_view(request):
+class CarsListView(ListView):
+    model = Car
+    template_name='cars.html'
+    context_object_name='cars'
     
-    if request.method == 'POST':
-        new_car_form=CarModelForm(request.POST,request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-            
+    def get_queryset(self):
+        cars=super().get_queryset().order_by('model')
+        search=self.request.GET.get('search')
         
-    else:
-        new_car_form=CarModelForm()
-    return render(request, 'new_car.html', { 'new_car_form': new_car_form })
+        if search:
+            filter = cars.filter(model__icontains=search)
+            return filter
+        else:
+            return cars
+        
+class NewCarCreateView(CreateView):
+    model= Car
+    form_class= CarModelForm
+    template_name= 'new_car.html'
+    success_url='/cars/'
+
+class CarDetailView(DetailView):
+    model=Car
+    template_name='car_detail.html'
+    
+        
+    
+        
