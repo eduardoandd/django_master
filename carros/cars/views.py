79e1f48389e5862d8f7a31afa92ae490267ddf34
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-09-26 13:07:37 -0300

    view update,delete e reverse lazy

--- carros/cars/views.py
+++ carros/cars/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render,redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
-from django.views.generic import ListView,CreateView,DetailView
+from django.views.generic import ListView,CreateView,DetailView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 
 
 class CarsListView(ListView):
@@ -32,6 +33,18 @@ class CarDetailView(DetailView):
     model=Car
     template_name='car_detail.html'
     
-        
+class CarUpdateView(UpdateView):
+    model=Car
+    form_class=CarModelForm
+    template_name='car_update.html'
+    
+    def get_success_url(self) -> str:
+        return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
+
+class CarDeleteView(DeleteView):
+     model = Car
+     template_name = 'car_delete.html'
+     success_url = '/cars'
+           
     
         
